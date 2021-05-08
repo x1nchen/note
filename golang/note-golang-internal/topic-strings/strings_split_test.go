ca0e6e72d83933f48f57abe48c19c8f7729c68da
x1nchen <xinchenccx@gmail.com>
2021-05-08 16:27:56 +0800

    fix

--- golang/note-golang-internal/topic-strings/strings_split_test.go
+++ golang/note-golang-internal/topic-strings/strings_split_test.go
@@ -5,13 +5,21 @@ import (
 	"strings"
 )
 
-func () {
+// Split 和 SplitAfter 差别是 SplitAfter 分割出来的字符尾部会带上分隔符
+func ExampleStringsSplit() {
 	image := "dockerhub.com/project/service"
 
 	fmt.Println(strings.SplitN(image, "/", 1))
 	fmt.Println(strings.SplitN(image, "/", 2))
-	fmt.Println(strings.Split(image, "/")) // [dockerhub.com project service]
+	fmt.Println(strings.Split(image, "/"))
 	fmt.Println(strings.SplitAfterN(image, "/", 1))
 	fmt.Println(strings.SplitAfterN(image, "/", 2))
-	fmt.Println(strings.SplitAfter(image, "/")) // [dockerhub.com/ project/ service]
+	fmt.Println(strings.SplitAfter(image, "/"))
+	// Output:
+	// [dockerhub.com/project/service]
+	// [dockerhub.com project/service]
+	// [dockerhub.com project service]
+	// [dockerhub.com/project/service]
+	// [dockerhub.com/ project/service]
+	// [dockerhub.com/ project/ service]
 }
