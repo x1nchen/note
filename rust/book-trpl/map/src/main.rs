4566d13a6b2ea36a040c7ed74fca3f897ce28e48
x1nchen <xinchenccx@gmail.com>
2021-06-19 17:39:25 +0800

    rust map

---
+++ rust/book-trpl/map/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    use std::collections::HashMap;
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    println!("{}, {}", field_name, field_value)
+}
