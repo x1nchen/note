ebfa6ec43e5a70e6b517e280d4468fbd2d7fd695
x1nchen <xinchenccx@gmail.com>
2021-06-21 17:20:16 +0800

    rust generics

--- rust/book-trpl/generics/src/main.rs
+++ rust/book-trpl/generics/src/main.rs
@@ -1,23 +1,16 @@
-fn largest(list: &[i32]) -> i32 {
-    let mut largest = list[0];
+struct Point<T> {
+    x: T,
+    y: T,
+}
 
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
     }
-
-    largest
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let p = Point { x: 5, y: 10 };
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    println!("p.x = {}", p.x());
 }
