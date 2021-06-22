637d014fd8cab432e2c772da738a9de6843aca2d
x1nchen <xinchenccx@gmail.com>
2021-06-22 11:15:21 +0800

    rust book-trpl

---
+++ rust/book-trpl/match-example/src/main.rs
@@ -0,0 +1,20 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {
+    let c = Coin::Penny;
+    println!("{}", value_in_cents(c))
+}
