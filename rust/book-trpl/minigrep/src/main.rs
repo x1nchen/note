ea9fa3832e3adfe7291c64badf2fd7f00e382799
x1nchen <xinchenccx@gmail.com>
2021-06-24 14:09:03 +0800

    rust project minigrep with program args

--- rust/book-trpl/minigrep/src/main.rs
+++ rust/book-trpl/minigrep/src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, fs, process};
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,27 +11,8 @@ fn main() {
 
     println!("Searching for {}", config.query);
     println!("In file: {}", config.filename);
-
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
